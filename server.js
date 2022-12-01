d4a581a0dba0f375ca73b496c63f60ad42952593
Quentin Jaffré <quentin.jaffre@epita.fr>
2022-12-01 15:58:46 +0100

    Ãrefactor: put configurations files in a config file.| doc: add a documentation of dev tools

--- server.js
+++ server.js
@@ -4,27 +4,24 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.listen(
-    4000, 
-    () => console.log("Time to Krummmble")
-)
+app.listen(4000, () => console.log('Time to Krummmble'));
 
-app.get("/associations", (req, res) => {
-    res.send("all the associations")
-})
+app.get('/associations', (req, res) => {
+  res.send('all the associations');
+});
 
-app.get("/associations/:id", (req,res) => {
-    res.send("this association")
-})
+app.get('/associations/:id', (req, res) => {
+  res.send('this association');
+});
 
-app.get("/associations/:tag", (req,res) => {
-    res.send("this association")
-})
+app.get('/associations/:tag', (req, res) => {
+  res.send('this association');
+});
 
-app.post("associations/:id", (req,res) => {
-    res.send("associations modify")
-})
+app.post('associations/:id', (req, res) => {
+  res.send('associations modify');
+});
 
-app.post("associations/create", (req,res) => {
-    res.send("create association")
-})
+app.post('associations/create', (req, res) => {
+  res.send('create association');
+});
